5c9ed08a887dcd5aec7f766e61e6da4b57fabcf5
Tyler <26290074+thegitduck@users.noreply.github.com>
2025-03-08 18:51:45 -0800

    typo

--- src/_data/feeds.ts
+++ src/_data/feeds.ts
@@ -2,6 +2,6 @@ export const rssFeedUrls = [
   "https://tylur.blog/api/rss.xml",
   "https://tkdodo.eu/blog/rss.xml",
   "https://overreacted.io/rss.xml",
-  "https://cassidoo.co/rss.xml"
+  "https://cassidoo.co/rss.xml",
   "https://waku.gg/api/rss.xml",
 ];
