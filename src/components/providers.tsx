f775aad2d4be8ace9796d7a5492505b2b6b72ac2
Tyler <26290074+thegitduck@users.noreply.github.com>
2025-03-11 23:33:54 -0700

    add code... needs to be client only though

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
-export const client = new QueryClient({
-  defaultOptions: {},
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
 });
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
+      {children}
+    </PersistQueryClientProvider>
+  );
 }
